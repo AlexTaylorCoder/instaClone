6f4609473f072486075d3e4ed4a564a6520dfca7
Derek Vogt <derekvogt3@gmail.com>
2022-07-15 09:56:12 -0400

    styling

--- src/subelements/PostPopUpComment.js
+++ src/subelements/PostPopUpComment.js
@@ -1,22 +1,29 @@
 import timeDiff from "../helperFunc/timeDiff";
 
-function PostPopUpComment({username,profPic,comment="",timestamp=0}) {
-    let displayDate
-    
-    if (timestamp) {
-        displayDate = timeDiff(timestamp)
+function PostPopUpComment({ username, profPic, comment = "", timestamp = 0 }) {
+  let displayDate;
 
-    }
+  if (timestamp) {
+    displayDate = timeDiff(timestamp);
+  }
 
-    return (
-        <>
-        <div className="flex-grid-postPopup">
-            <img className = "profile-picture fit-img" width="50px" height="50px" src={profPic}/>
-            <p className="flex-grid-align-time"><b>{username}</b>{timestamp ? displayDate: ""}</p>
-            <p>{comment}</p>
-        </div>
-        </>
-        ) 
+  return (
+    <>
+      <div className="flex-grid-postPopup">
+        <img
+          className="profile-picture fit-img"
+          width="50px"
+          height="50px"
+          src={profPic}
+        />
+        <p className="flex-grid-align-time">
+          <b>{username}</b>
+          {timestamp ? displayDate : ""}
+        </p>
+        <p>{comment}</p>
+      </div>
+    </>
+  );
 }
 
-export default PostPopUpComment
+export default PostPopUpComment;
